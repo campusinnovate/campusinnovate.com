819e3ad4ea61db5f40c36b4a2b1b841fabd8ec57
campusinnovate <innovatecampus@gmail.com>
2026-08-21 20:37:38 +0700

    Show site navigation on the Pages root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,13 @@
-export { default } from './(sites)/home/page';
+import Navbar from '@/components/layout/navbar';
+import Footer from '@/components/layout/footer';
+import Homepage from '@/components/public/Homepage';
+
+export default function RootPage() {
+  return (
+    <div className="public-site min-h-screen">
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
